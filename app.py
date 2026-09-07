81faf93b284ca22d0457f21603c657c3a38056c9
M-Muneeb87 <195584418+M-Muneeb87@users.noreply.github.com>
2026-09-07 21:57:04 +0500

    Refactor imports and improve PDF processing logic

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
